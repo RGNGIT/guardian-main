e4f80af2e9982581861022df5ff11911a350a068
RGNGIT <vnifedow@bk.ru>
2022-10-23 19:03:39 +0500

    ///

--- api-gateway/src/api/service/dashboard.ts
+++ api-gateway/src/api/service/dashboard.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { Guardians } from '@helpers/guardians';
+import { Users } from '@helpers/users';
 
 /**
  * Dashboard route
@@ -7,5 +8,6 @@ import { Guardians } from '@helpers/guardians';
 export const dashboardAPI = Router();
 
 dashboardAPI.get('/test', (req: Request, res: Response) => {
+    
     res.send('Dashboard works');
 });
