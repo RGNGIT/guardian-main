7622d5084c642578ebc45f3e8ac05e964ce6f3dd
RGNGIT <vnifedow@bk.ru>
2022-10-24 14:30:41 +0500

    +

--- api-gateway/src/api/service/dashboard.ts
+++ api-gateway/src/api/service/dashboard.ts
@@ -1,13 +1,22 @@
 import { Request, Response, Router } from 'express';
 import { Guardians } from '@helpers/guardians';
 import { Users } from '@helpers/users';
+import { Logger } from '@guardian/common';
+import { ResponseCode, formResponse } from '@helpers/response-manager';
 
 /**
  * Dashboard route
  */
 export const dashboardAPI = Router();
 
-dashboardAPI.get('/test', (req: Request, res: Response) => {
-    
-    res.send('Dashboard works');
+dashboardAPI.get('/devices/:policyId', (req: Request, res: Response) => {
+    const guardians = new Guardians();
+    try {
+        const policyId = req.params.policyId;
+        
+        res.send('Dashboard works');
+    } catch (error) {
+        new Logger().error(error, ['API_GATEWAY']);
+        res.status(500).send(formResponse(-1, error.message, ''));
+    }
 });
