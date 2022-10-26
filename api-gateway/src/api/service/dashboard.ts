7db01d813219daca1d4b9e65844538a385969f51
Rageon <vnifedow@bk.ru>
2022-10-26 23:34:47 +0500

    Dashboard

--- api-gateway/src/api/service/dashboard.ts
+++ api-gateway/src/api/service/dashboard.ts
@@ -3,6 +3,43 @@ import { Guardians } from '@helpers/guardians';
 import { Logger, AuthenticatedRequest } from '@guardian/common';
 import { ResponseCode, formResponse } from '@helpers/response-manager';
 
+interface IDashboardDevice {
+    deviceId: string;
+    status: string;
+    currentEmission: {value: string, trend: string};
+    totalEmission: {value: string, trend: string};
+}
+
+let prevValues: IDashboardDevice[] = null;
+
+function resolveDistinctDeviceIds(records): string[] {
+    let temp: string[] = [];
+    for(const record of records) {
+        if(!temp.includes(record['document']['issuer'] as string)) {
+            temp.push(record['document']['issuer'] as string);
+        }
+    }
+    return temp;
+}
+
+function resolveDevices(records, distinctDeviceIds): IDashboardDevice[] {
+    const temp: IDashboardDevice[] = [];
+    for(const id of distinctDeviceIds) {
+        temp.push({
+            deviceId: id,
+            status: null,
+            currentEmission: {value: null, trend: null},
+            totalEmission: {value: null, trend: null},
+        });
+        for(const record of records) {
+            if(record['document']['issuer'] == id) {
+
+            }
+        }
+    }
+    return temp;
+}
+
 /**
  * Dashboard route
  */
@@ -13,16 +50,11 @@ dashboardAPI.get('/devices/:policyId', async (req: AuthenticatedRequest, res: Re
     try {
         const policyId = req.params.policyId;
         const type = req.query.type;
-        let distinctDeviceIds: string[] = [];
         const records = await guardians.getVcDocuments({type, "document.credentialSubject.policyId": policyId});
-        /*
-        for(const record of records) {
-            if(!distinctDeviceIds.includes(record['document']['issuer'] as string)) {
-
-            }
-        }
-        */
-        res.send(records);
+        const distinctDeviceIds = resolveDistinctDeviceIds(records);
+        const devices = resolveDevices(records, distinctDeviceIds);
+        prevValues = devices;
+        res.json(devices);
     } catch (error) {
         new Logger().error(error, ['API_GATEWAY']);
         res.status(500).send(formResponse(ResponseCode.GET_DASHBOARD_FAIL, error.message, 'GET_DASHBOARD_FAIL'));
