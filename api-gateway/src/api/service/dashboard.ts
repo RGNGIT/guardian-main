cd27df891da297589a9cfcaf15127b561c071b61
RGNGIT <vnifedow@bk.ru>
2022-10-27 05:19:54 +0500

    db

--- api-gateway/src/api/service/dashboard.ts
+++ api-gateway/src/api/service/dashboard.ts
@@ -3,14 +3,36 @@ import { Guardians } from '@helpers/guardians';
 import { Logger, AuthenticatedRequest } from '@guardian/common';
 import { ResponseCode, formResponse } from '@helpers/response-manager';
 
+enum EmissionTypes {
+    CURRENT_EMISSION = 0,
+    TOTAL_EMISSION = 1
+};
+
 interface IDashboardDevice {
     deviceId: string;
     status: string;
-    currentEmission: {value: string, trend: string};
-    totalEmission: {value: string, trend: string};
+    currentEmission: {value: number, trend: string};
+    totalEmission: {value: number, trend: string};
 }
 
-let prevValues: IDashboardDevice[] = null;
+let prevDeviceValues: IDashboardDevice[] = null;
+
+function resolveTrend(id, emissionType, value): string {
+    if(!prevDeviceValues) return null;
+    for(const device of prevDeviceValues) {
+        if(device.deviceId === id) {
+            switch(emissionType) {
+                case EmissionTypes.CURRENT_EMISSION:
+                    if(device.currentEmission == value) return 'still';
+                    return device.currentEmission > value ? 'down' : 'up';
+                case EmissionTypes.TOTAL_EMISSION:
+                    if(device.totalEmission == value) return 'still';
+                    return device.totalEmission > value ? 'down' : 'up';
+            }
+        }
+    }
+    return null;
+}
 
 function resolveDistinctDeviceIds(records): string[] {
     let temp: string[] = [];
@@ -27,15 +49,19 @@ function resolveDevices(records, distinctDeviceIds): IDashboardDevice[] {
     for(const id of distinctDeviceIds) {
         temp.push({
             deviceId: id,
-            status: null,
-            currentEmission: {value: null, trend: null},
-            totalEmission: {value: null, trend: null},
+            status: 'Active', // TODO: Resolve status
+            currentEmission: {value: 0, trend: null},
+            totalEmission: {value: 0, trend: null},
         });
+        const item = temp[temp.length - 1];
         for(const record of records) {
-            if(record['document']['issuer'] == id) {
-
+            if(record['document']['issuer'] === id) {
+                // TODO: Correct calculations
+                item.currentEmission.value += Number(record['document']['credentialSubject']['field1']);
             }
         }
+        item.currentEmission.trend = resolveTrend(item.deviceId, EmissionTypes.CURRENT_EMISSION, item.currentEmission.value);
+        item.totalEmission.trend = resolveTrend(item.deviceId, EmissionTypes.TOTAL_EMISSION, item.totalEmission.value);
     }
     return temp;
 }
@@ -53,7 +79,7 @@ dashboardAPI.get('/devices/:policyId', async (req: AuthenticatedRequest, res: Re
         const records = await guardians.getVcDocuments({type, "document.credentialSubject.policyId": policyId});
         const distinctDeviceIds = resolveDistinctDeviceIds(records);
         const devices = resolveDevices(records, distinctDeviceIds);
-        prevValues = devices;
+        prevDeviceValues = devices;
         res.json(devices);
     } catch (error) {
         new Logger().error(error, ['API_GATEWAY']);
